568beca3dafbbf110961621434c33c563ad1e653
Jani <jani.n@icloud.com>
2022-11-14 15:59:34 +0200

    Test

---
+++ src/main/java/PalvelinProjekti/PalvelinProjekti/PalvelinProjektiApplication.java
@@ -0,0 +1,13 @@
+package PalvelinProjekti.PalvelinProjekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalvelinProjektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalvelinProjektiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PalvelinProjekti/PalvelinProjekti/PalvelinProjektiApplicationTests.java
@@ -0,0 +1,13 @@
+package PalvelinProjekti.PalvelinProjekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalvelinProjektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
